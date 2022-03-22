0e7b02609381401699071610002f8238969d6cc9
hugosenari <hugosenari@gmail.com>
2022-03-22 00:37:31 -0300

    chore: re-index ../nimpkgs/s/simpledecimal

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-simpledecimal-master.flake = false;
   inputs.src-simpledecimal-master.owner = "pigmej";
-  inputs.src-simpledecimal-master.ref   = "refs/heads/master";
+  inputs.src-simpledecimal-master.ref   = "master";
   inputs.src-simpledecimal-master.repo  = "nim-simple-decimal";
   inputs.src-simpledecimal-master.type  = "github";
   
